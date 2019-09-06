4356391b2728d18bd514a27b07a917b2bb8d0059
manuXD270516 <manueljss@live.com>
2019-09-06 10:19:20 -0400

    Creacion del proyecto Delta Cargo SRL

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DeltaCargoSRL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
